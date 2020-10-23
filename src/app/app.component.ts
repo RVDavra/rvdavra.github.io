edf58dda093f0b174fb2b2f9b0fa8d2ed857bb19
Ritesh Davra <riteshdavra@gmail.com>
2020-10-23 14:25:07 +0530

    add project tiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MetadataModel, RootMetaDataModel } from './models/metadata-model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'project1';
+export class AppComponent implements OnInit {
+  public projects: MetadataModel[] = [];
+  constructor(public http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<RootMetaDataModel>('assets/data.json')
+    .subscribe(data => this.projects = data.projects);
+  }
 }
---
+++ src/app/models/metadata-model.ts
@@ -0,0 +1,8 @@
+export class MetadataModel {
+    name: string;
+    url: string;
+}
+
+export class RootMetaDataModel {
+    projects: MetadataModel[];
+}
